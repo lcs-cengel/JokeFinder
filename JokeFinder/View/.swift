844b2a749af0cbbe5a2dbcf9e56133ff8a224068
Connor Engel <cengel@lcs.on.ca>
2025-03-04 11:37:21 -0500

    Added a very basic view to show the current joke.

---
+++ JokeFinder/View/.swift
@@ -0,0 +1,43 @@
+//
+//  JokeView.swift
+//  JokeFinder
+//
+//  Created by Connor Engel on 2025-03-04.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    
+    // MARK: Stored properties
+    
+    // Create the view model (temporarily show the default joke)
+    @State var viewModel = JokeViewModel(currentJoke: exampleJoke)
+    
+    // MARK: Computed properties
+    var body: some View {
+        VStack {
+            
+            // Show a joke if one exists
+            if let currentJoke = viewModel.currentJoke {
+                
+                Group {
+                    Text(currentJoke.setup ?? "")
+                        .padding(.bottom, 100)
+                    
+                    Text(currentJoke.punchline ?? "")
+ 
+                }
+                .font(.title)
+                .multilineTextAlignment(.center)
+                
+            }
+            
+        }
+    }
+}
+ 
+#Preview {
+    JokeView()
+}
+
