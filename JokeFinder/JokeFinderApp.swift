f71ce1e8f58266300e76283c814a8e41570fbd83
Connor Engel <cengel@lcs.on.ca>
2025-03-04 11:08:51 -0500

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Connor Engel on 2025-03-04.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
